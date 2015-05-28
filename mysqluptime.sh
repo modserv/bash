d41cf275c028079f89ac80da6774149dac8e7fd0
modserv <support@modserv.com>
2015-05-28 04:37:58 +0200

    Update mysqluptime.sh

--- mysqluptime.sh
+++ mysqluptime.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-
 _MYSQLUP=`mysql -e 'SHOW STATUS;' | grep -i uptime | head -1 | awk {' print $2 '}`
 echo "scale=2; $_MYSQLUP / 60 / 60" | bc | awk '{printf "%.2f\n", $0}'
+
+echo "Thanks"
