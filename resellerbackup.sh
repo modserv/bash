dc232088af6ab01d8874cef156c40adc2a76bd07
modserv <support@modserv.com>
2015-08-02 17:26:24 +0200

    Update resellerbackup.sh

--- resellerbackup.sh
+++ resellerbackup.sh
@@ -5,5 +5,5 @@ read RESOWNER
 
 for _users in `grep $RESOWNER /var/cpanel/users/* | cut -d":" -f1 | cut -d"/" -f 5 | uniq`
   do
-    /usr/local/cpanel/pkgacct $_users
+    /usr/local/cpanel/scripts/pkgacct $_users
   done
